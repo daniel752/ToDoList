598249dbd0ab6eb6583976b05f44e9ecd3f7d139
daniel752 <daniel4800@gmail.com>
2023-07-02 11:38:50 +0300

    Trying to fix incompatability with mongoose and node

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const app = express();
 const port = 8000;
 const mongoUrl = process.env.DB_URL;
